938d7cfa90e9a47450f7e8ad0b034a294487a970
msg1999 <71818408+msg1999@users.noreply.github.com>
2021-01-21 10:32:27 +0100

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "rootroot";
+	$database = "webpedidos";
+
+	try {
+		$conexion = new PDO("mysql:host=$servername;dbname=$database", $username, $password); 	 	 	 	 	 	
+		$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 	 	 	 	 	 	
+	} catch (PDOException $ex) {
+		echo $ex->getMessage(); 	 	 	 	 	 	
+	}
+?>
