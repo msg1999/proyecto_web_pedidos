e49c923325c1c3e4c2b544caac1a1bd4a0360a14
Daniel González Carretero <71818186+daniel-gonzalez-carretero@users.noreply.github.com>
2021-01-21 10:40:38 +0100

    📦 Apartado 1

---
+++ funciones.php
@@ -0,0 +1,25 @@
+<?php 
+
+include_once 'conexion.php';
+
+function obtenerAcceso($username, $password)  {
+	// Daniel González Carretero
+	// La función comprueba si existe un usuario $username con una contraseña $password
+	// Devuelve el ID del usuario, si el usuario y contraseña son correctos, NULL si no lo son.
+
+	global $conexion;
+
+	try {
+		$obtenerID = $conexion->preprare("SELECT id FROM admin WHERE username = :username && passcode = :password");
+		$obtenerID->bindParam(":username", $username);
+		$obtenerID->bindParam(":password", $password);
+		$obtenerID->execute(PDO::FETCH_ASSOC);
+
+		return $obtenerID->fetch(PDO:);
+	} catch (PDOException $ex) {
+		echo "<strong>ERROR: </strong> ". $ex->getMessage();
+	}
+
+}
+
+?>
