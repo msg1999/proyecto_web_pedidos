ba07ef40de0e6f57e8693b6f6b656b92ab6f592f
Daniel González Carretero <71818186+daniel-gonzalez-carretero@users.noreply.github.com>
2021-01-25 13:57:54 +0100

    ✏ Se deja únicamente la función usada

--- funciones.php
+++ funciones.php
@@ -2,26 +2,6 @@
 
 include_once 'conn.php';
 
-function obtenerAcceso($username, $password)  {
-	// Daniel González Carretero
-	// La función comprueba si existe un usuario $username con una contraseña $password
-	// Devuelve el ID del usuario, si el usuario y contraseña son correctos, NULL si no lo son.
-
-	global $conexion;
-
-	try {
-		$obtenerID = $conexion->prepare("SELECT id FROM admin WHERE username = :username && passcode = :password");
-		$obtenerID->bindParam(":username", $username);
-		$obtenerID->bindParam(":password", $password);
-		$obtenerID->execute();
-
-		return $obtenerID->fetch(PDO::FETCH_ASSOC)["id"];
-	} catch (PDOException $ex) {
-		echo "<strong>ERROR: </strong> ". $ex->getMessage();
-	}
-
-}
-
 function consultarTotalVentas($fechaInicioBusqueda, $fechaFinBusqueda) {
 	// Daniel González Carretero
 	// La función consulta todas las ventas realizadas entre las fechas $fechaInicioBusqueda y $fechaFinBusqueda
