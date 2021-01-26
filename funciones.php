6d4e5885e9a9547b9dc23146bac906b66bc4b213
Daniel González Carretero <71818186+daniel-gonzalez-carretero@users.noreply.github.com>
2021-01-26 13:59:11 +0100

    ✏Corregido nombre variable Conexion

--- funciones.php
+++ funciones.php
@@ -6,10 +6,10 @@
     // La función consulta todos los pagos realizados entre las fechas $fechaInicioBusqueda y $fechaFinBusqueda
     // Devuelve un array con los pagos realizados, o NULL si ha habido algún error / no hay pagos entre esas fechas
     
-    global $conexion;
+    global $conn;
 
     try {
-        $obtenerVentas = $conexion->prepare("SELECT paymentDate, amount FROM payments WHERE customerNumber = :usuario AND payments.paymentDate >= :fechaInicio AND payments.paymentDate <= :fechaFin");
+        $obtenerVentas = $conn->prepare("SELECT paymentDate, amount FROM payments WHERE customerNumber = :usuario AND payments.paymentDate >= :fechaInicio AND payments.paymentDate <= :fechaFin");
         $obtenerVentas->bindParam(":fechaInicio", $fechaInicioBusqueda);
         $obtenerVentas->bindParam(":fechaFin", $fechaFinBusqueda);
         $obtenerVentas->bindParam(":usuario", $usuarioBusqueda);
