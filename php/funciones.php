7f887f33a0fc195d848b6e9481e6d531fb173f0f
msg1999 <71818408+msg1999@users.noreply.github.com>
2021-01-26 12:10:25 +0000

    Update funciones.php

--- php/funciones.php
+++ php/funciones.php
@@ -2,7 +2,7 @@
     //Code refactor by: Marco Santiago
     include_once('conn.php');
 
-    function get_productos(){
+	function get_productos(){
         // Dev by: Marco Santiago.
 	// Function: La función obtiene los nombres de todos los productos.
 	// Return: array de productos.
@@ -85,5 +85,23 @@
         }
 
     }
+	function consultarTotalVentas($fechaInicioBusqueda, $fechaFinBusqueda, $usuarioBusqueda) {
+	// Daniel González Carretero
+	// La función consulta todas las ventas realizadas por un usuario entre las fechas $fechaInicioBusqueda y $fechaFinBusqueda
+	// Devuelve un array con las compras realizadas, o NULL si ha habido algún error / no hay ventas entre esas fechas
+	
+	global $conn;
+
+	try {
+		$obtenerVentas = $conn->prepare("SELECT productName AS 'nombre', priceEach AS 'precio', COUNT(productName) AS 'unidades' FROM orderdetails LEFT JOIN products ON orderdetails.productcode = products.productcode LEFT JOIN orders ON orders.ordernumber = orderdetails.ordernumber WHERE orders.orderdate >= :fechaInicio AND orders.orderdate <= :fechaFin AND orders.customernumber = :usuario GROUP BY productName");
+		$obtenerVentas->bindParam(":fechaInicio", $fechaInicioBusqueda);
+		$obtenerVentas->bindParam(":fechaFin", $fechaFinBusqueda);
+		$obtenerVentas->bindParam(":usuario", $usuarioBusqueda);
+		$obtenerVentas->execute();
+
+		return $obtenerVentas->fetchAll(PDO::FETCH_ASSOC);
+	} catch (PDOException $ex) {
+		return null;
+	}
 
 ?>
