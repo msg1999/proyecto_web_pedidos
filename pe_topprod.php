37dac41a1d52651e69bf28512673f679862a2335
Daniel González Carretero <71818186+daniel-gonzalez-carretero@users.noreply.github.com>
2021-01-26 12:24:11 +0100

    ✏ Actualizado archivo acorde a funciones.php
    Se actualiza el fichero para obligar al usuario a estar iniciado sesión, para poder filtrar las ventas por su usuario.

--- pe_topprod.php
+++ pe_topprod.php
@@ -1,4 +1,26 @@
-<!DOCTYPE html>
+<?php 
+	session_start();
+	include_once 'funciones.php';
+
+	if (isset($_SESSION) && !empty($_SESSION) && isset($_SESSION["idUsuario"])) {
+
+		$debeMostrarse = false;
+
+		if (isset($_POST) && !empty($_POST) && isset($_POST["fechaInicio"]) && isset($_POST["fechaFin"])) {
+			$fechaInicio = $_POST["fechaInicio"];
+			$fechaFin = $_POST["fechaFin"];
+			$usuario = $_SESSION["idUsuario"];
+
+			$ventas = consultarTotalVentas($fechaInicio, $fechaFin, $usuario);
+			$debeMostrarse = true;
+
+		}
+
+	} else {
+		echo "<p>Parece que tienes que iniciar sesión primero... Haz <a href='pe_login.php'>click aquí</a> para iniciar sesión</p>"; // En caso de que no se redireccione automáticamente
+		header("location: pe_login.php");
+	}
+?>
 <html>
 <head>
 	<title>Consultar ventas totales</title>
@@ -18,25 +40,17 @@
 	</form>
 
 	<?php 
-
-		include_once 'funciones.php';
-
-		if (isset($_POST) && !empty($_POST) && isset($_POST["fechaInicio"]) && isset($_POST["fechaFin"])) {
-			$fechaInicio = $_POST["fechaInicio"];
-			$fechaFin = $_POST["fechaFin"];
-
-			$ventas = consultarTotalVentas($fechaInicio, $fechaFin);
-
+		if ($debeMostrarse) {
 			if ($ventas == null) {
 				echo "<p>Parece que no ha habido ninguna venta sobre esas fechas...</p>";
 			} else {
 				echo "<table border='1' cellpadding='3'><tr><th>Nombre del Producto</th><th>Precio de cada Unidad</th><th>Unidades Vendidas</th></tr>";
+
 				foreach ($ventas as $venta) {
 					echo "<tr><td>" . $venta["nombre"] . "</td><td>" . $venta["precio"] . "</td><td>" . $venta["unidades"] . "</td></tr>";
 				}
 			}
 		}
-
 	?>
 
 </body>
