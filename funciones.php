3e88c4081038546f8d0f8ba73f142d4dfe0d85f2
Daniel González Carretero <71818186+daniel-gonzalez-carretero@users.noreply.github.com>
2021-01-26 12:23:13 +0100

    ✏ Solucionada duda sobre SELECT
    Ahora se muestran todas las ventas realizadas por un único cliente, en vez de por todos los clientes.

--- funciones.php
+++ funciones.php
@@ -2,17 +2,18 @@
 
 include_once 'conn.php';
 
-function consultarTotalVentas($fechaInicioBusqueda, $fechaFinBusqueda) {
+function consultarTotalVentas($fechaInicioBusqueda, $fechaFinBusqueda, $usuarioBusqueda) {
 	// Daniel González Carretero
-	// La función consulta todas las ventas realizadas entre las fechas $fechaInicioBusqueda y $fechaFinBusqueda
+	// La función consulta todas las ventas realizadas por un usuario entre las fechas $fechaInicioBusqueda y $fechaFinBusqueda
 	// Devuelve un array con las compras realizadas, o NULL si ha habido algún error / no hay ventas entre esas fechas
 	
 	global $conexion;
 
 	try {
-		$obtenerVentas = $conexion->prepare("SELECT productName AS 'nombre', priceEach AS, COUNT(productName) AS 'unidades' FROM orderdetails LEFT JOIN products ON orderdetails.productcode = products.productcode LEFT JOIN orders ON orders.ordernumber = orderdetails.ordernumber WHERE orders.orderdate >= :fechaInicio AND orders.orderdate <= :fechaFin GROUP BY productName");
+		$obtenerVentas = $conexion->prepare("SELECT productName AS 'nombre', priceEach AS 'precio', COUNT(productName) AS 'unidades' FROM orderdetails LEFT JOIN products ON orderdetails.productcode = products.productcode LEFT JOIN orders ON orders.ordernumber = orderdetails.ordernumber WHERE orders.orderdate >= :fechaInicio AND orders.orderdate <= :fechaFin AND orders.customernumber = :usuario GROUP BY productName");
 		$obtenerVentas->bindParam(":fechaInicio", $fechaInicioBusqueda);
 		$obtenerVentas->bindParam(":fechaFin", $fechaFinBusqueda);
+		$obtenerVentas->bindParam(":usuario", $usuarioBusqueda);
 		$obtenerVentas->execute();
 
 		return $obtenerVentas->fetchAll(PDO::FETCH_ASSOC);
