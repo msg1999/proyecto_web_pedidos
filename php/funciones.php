657b28e40bddafffa131c4472a72ba733d7e609e
msg1999 <71818408+msg1999@users.noreply.github.com>
2021-02-01 12:06:41 +0000

    Update funciones.php

--- php/funciones.php
+++ php/funciones.php
@@ -164,5 +164,24 @@
 	}
 
       }
+  function consultarTotalPagos($fechaInicioBusqueda, $fechaFinBusqueda, $usuarioBusqueda) {
+    // Daniel González Carretero
+    // La función consulta todos los pagos realizados entre las fechas $fechaInicioBusqueda y $fechaFinBusqueda
+    // Devuelve un array con los pagos realizados, o NULL si ha habido algún error / no hay pagos entre esas fechas
+    
+    global $conn;
+
+    try {
+        $obtenerVentas = $conn->prepare("SELECT paymentDate, amount FROM payments WHERE customerNumber = :usuario AND payments.paymentDate >= :fechaInicio AND payments.paymentDate <= :fechaFin");
+        $obtenerVentas->bindParam(":fechaInicio", $fechaInicioBusqueda);
+        $obtenerVentas->bindParam(":fechaFin", $fechaFinBusqueda);
+        $obtenerVentas->bindParam(":usuario", $usuarioBusqueda);
+        $obtenerVentas->execute();
+
+        return $obtenerVentas->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $ex) {
+        return null;
+    }
+}
 
 ?>
